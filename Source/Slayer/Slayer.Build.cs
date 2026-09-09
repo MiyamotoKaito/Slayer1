3201a2dc204514b009cc5b11267b237348f014ee
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-09-10 04:46:24 +0900

    [add]GASの導入

--- Source/Slayer/Slayer.Build.cs
+++ Source/Slayer/Slayer.Build.cs
@@ -10,7 +10,7 @@ public class Slayer : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {"GameplayAbilities", "GameplayTasks", "GamePlayTags"  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
