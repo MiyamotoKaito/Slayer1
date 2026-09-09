4b3556f2e304f63d3227138c404df797c65b026c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-09-10 04:46:54 +0900

    [update]ディレクトリの移動とコンポーネントの初期化

---
+++ Source/Slayer/Private/GameAbilitySystem/Character/TestCharacter.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCharacter.h"
+
+
+// Sets default values
+ATestCharacter::ATestCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	// AbilitySystemComponentを追加する
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	// ネットワークでのEffectの共有をできるように設定
+	AbilitySystemComponent->SetIsReplicated(true);
+	// 共有するタイプ
+	AbilitySystemComponent->SetReplicationMode(ReplicationMode);
+}
+
+// Called when the game starts or when spawned
+void ATestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+UAbilitySystemComponent* ATestCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ATestCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+		
+	if (AbilitySystemComponent != nullptr)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this,this);
+	}
+}
+
+void ATestCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	
+	if (AbilitySystemComponent != nullptr)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this,this);
+	}
+}
+
+// Called to bind functionality to input
+void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Slayer/Private/GameAbilitySystem/Character/TestCharacter.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "TestCharacter.generated.h"
+
+UCLASS()
+class SLAYER_API ATestCharacter : public ACharacter,public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ATestCharacter();
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Abilities")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+protected:
+
+	virtual void BeginPlay() override;
+	/*
+	 * プレイヤーを操作するときに使う関数
+	 */
+	virtual void OnRep_PlayerState() override;
+	/*
+	 * 主に、敵がコントローラーに依存されている場合に使う関数
+	 */
+	virtual void PossessedBy(AController* NewController) override;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Abilities")
+	EGameplayEffectReplicationMode ReplicationMode = EGameplayEffectReplicationMode:: Mixed;
+};
