840122703f22b12ec5cf127d8825987e1f2915b8
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-09-22 14:31:01 +0900

    [update]AttributeSetの実装

--- Source/Slayer/GameAbilitySystem/Attribute/BasicAttributeSet.cpp
+++ Source/Slayer/GameAbilitySystem/Attribute/BasicAttributeSet.cpp
@@ -1,4 +1,43 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MyClass.h"
+#include "BasicAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+UBasicAttributeSet::UBasicAttributeSet()
+{
+	Health = 100;
+	MaxHealth = 100;
+	Stamina = 100;
+	MaxStamina = 100;
+}
+
+void UBasicAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Health, OldValue);
+}
+
+void UBasicAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxHealth, OldValue);
+}
+
+void UBasicAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Stamina, OldValue);
+}
+
+void UBasicAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldValue) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldValue);
+}
+
+void UBasicAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBasicAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
--- Source/Slayer/GameAbilitySystem/Attribute/BasicAttributeSet.h
+++ Source/Slayer/GameAbilitySystem/Attribute/BasicAttributeSet.h
@@ -4,13 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
-#include "MyClass.generated.h"
+#include "AbilitySystemComponent.h"
+#include "BasicAttributeSet.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class SLAYER_API UMyClass : public UAttributeSet
+class SLAYER_API UBasicAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	// 体力関連のAttribute
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Abilities", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, Health);
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Abilities", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, MaxHealth);
+	
+	// スタミナ関連のAttribute
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Abilities", ReplicatedUsing = OnRep_Stamina)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, Stamina);
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Abilities", ReplicatedUsing = OnRep_MaxStamina)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, MaxStamina);
+	
+public:
+	UBasicAttributeSet();
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldValue) const;
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldValue) const;
+
+	/**
+	 * サーバーの同期リストに設定する
+	 * @param OutLifetimeProps 
+	 */
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 };
